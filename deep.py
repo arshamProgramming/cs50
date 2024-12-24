d1807ef4ade824b428864b3f1d0ddeb55a291b22
arshamProgramming <arshamGithubPython@gmail.com>
2024-12-24 07:37:32 -0800

    Delete deep.py

--- deep.py
+++
@@ -1,9 +0,0 @@
-inp =input('Great Question of Life, the Universe and Everything ').strip().lower()
-if inp =='42':
-    print('yes')
-elif inp =='forty two' :
-    print('yes')
-elif inp == 'forty-two':
-    print('yes')
-else:
-    print('no')
